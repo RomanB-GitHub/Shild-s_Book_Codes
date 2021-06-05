55f099888befb3f9aee50cacb2a8c5b69be0ad5f
Roman Bovbalan <73779111+RomanB-GitHub@users.noreply.github.com>
2021-06-05 23:44:35 +0300

    Create Dinamic_Inicialization.java
    The code was added!

---
+++ Dinamic_Inicialization.java
@@ -0,0 +1,10 @@
+public class Dinamic_Inicialization {
+    public static void main(String[] args) {
+// узнаем гипотенузу прямоугольного треугольника
+        double a = 3.0, b = 4.0;
+
+        double c = Math.sqrt(a * a + b * b);
+        System.out.println("Гипотенуза равна  = " + c);
+
+    }
+}
