9c196e25dff6758981e3330ddd4a36d1f3bffc8f
Roman Bovbalan <73779111+RomanB-GitHub@users.noreply.github.com>
2021-06-05 23:40:12 +0300

    Create BooleanTest.java
    Code was added!

---
+++ BooleanTest.java
@@ -0,0 +1,19 @@
+public class BooleanTest {
+    public static void main(String[] args) {
+
+        boolean b;
+
+        b = false;
+        System.out.println("b равно: " + b);
+        b = true;
+        System.out.println("а теперь b равно: " + b);
+
+        if (b) System.out.println("код выполняеться!");
+        b = false;
+        if (b) System.out.println("код не выполняеться!");
+
+        System.out.println("10 > 9 равно :" + (10 > 9));
+
+
+    }
+}
