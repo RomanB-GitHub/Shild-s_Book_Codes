922e2c3c01929415d5b8aab443cab5126dbe57d0
Roman Bovbalan <73779111+RomanB-GitHub@users.noreply.github.com>
2021-06-05 23:42:42 +0300

    Create CharDemo2.java
    The second type of  CharDemo code was created!

---
+++ CharDemo2.java
@@ -0,0 +1,12 @@
+public class CharDemo2 {
+    public static void main(String[] args) {
+
+        char ch1;
+
+        ch1 = 'X';
+        System.out.println("Сейчас ch1 будет : " + ch1);
+
+        ch1++ ;
+        System.out.println("Теперь ch1 инкрементировано на еденицу и будет уже по другому : " + ch1);
+    }
+}
