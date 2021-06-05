04d2c17613675ba5eab6744fcce6e33545bbe387
Roman Bovbalan <73779111+RomanB-GitHub@users.noreply.github.com>
2021-06-05 23:41:19 +0300

    Create CharDemo.java
    YThe First type of code!

---
+++ CharDemo.java
@@ -0,0 +1,11 @@
+public class CharDemo {
+
+    public static void main(String[] args) {
+
+        char x, y;
+        x = 88;
+        y = 'Y';
+
+        System.out.println( "Литерал \"х\" будет : " + x + "\nЛитерал \"у\" будет : "+ y );
+    }
+}
