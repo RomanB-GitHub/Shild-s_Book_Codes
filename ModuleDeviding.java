0cf25c77bc16e9142c9f7f1b0c357022e1ddc6ef
Bovbalan Roman <insekticud@gmail.com>
2021-06-09 00:07:18 +0300

    Was created a class for demonstrating of demonstaring.

---
+++ ModuleDeviding.java
@@ -0,0 +1,11 @@
+// демонстрация  операции деления по модулю  "%"
+
+public class ModuleDeviding {
+    public static void main(String[] args) {
+        int x = 42;
+        double y = 42.25;
+
+        System.out.println("x mod 10 = " + x % 10);
+        System.out.println("y = 10 = " + y % 10);
+    }
+}
