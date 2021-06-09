dfbb020c029349eb49533323b691259c7833fe60
Bovbalan Roman <insekticud@gmail.com>
2021-06-09 12:54:22 +0300

    Demonstrating of increment

---
+++ IncrementDecrement.java
@@ -0,0 +1,18 @@
+// демонстрация операции декремента
+
+public class IncrementDecrement {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c, d;
+        c = ++b;
+        d = a++;
+        c++;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+    }
+}
