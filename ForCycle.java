18bc66a878f78dbe84de712a94cbe74d3b65844f
Bovbalan Roman <insekticud@gmail.com>
2021-06-13 19:18:40 +0300

    Cycle for example

---
+++ ForCycle.java
@@ -0,0 +1,19 @@
+/**
+ * Демонстрация применения цикла for
+ */
+public class ForCycle {
+    public static void main(String[] args) {
+        int n;
+
+        for (n = 10; n > 0; n--)
+            System.out.println("Такт " + n);
+    }
+}
+
+
+//class ForTick {
+//    public static void main(String args[]) {
+//        // здесь переменная n объявляется в самом цикле for
+//        for (int n = 1O; n > O; n--)
+//            System.out.println("тaкт " + n);
+//    }
