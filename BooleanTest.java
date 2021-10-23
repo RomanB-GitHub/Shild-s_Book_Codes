36c4f246b59b325bbeb89b9b8dd58c0e7905e258
Roman Bovbalan <insekticud@gmail.com>
2021-10-23 09:16:51 +0300

    the booleanTest was re_coded...

--- BooleanTest.java
+++ BooleanTest.java
@@ -1,20 +1,26 @@
+// продемонстрировать применение типа boolean
 
 public class BooleanTest {
-    public static void main(String[] args) {
 
-        boolean b;
+  public static void main(String[] args) {
 
-        b = false;
-        System.out.println("b равно: " + b);
-        b = true;
-        System.out.println("а теперь b равно: " + b);
+    boolean b;
 
-        if (b) System.out.println("код выполняеться!");
-        b = false;
-        if (b) System.out.println("код не выполняеться!");
+    b = false;
+    System.out.println(" b равно " + b);
 
-        System.out.println("10 > 9 равно :" + (10 > 9));
+    b = true;
+    System.out.println("теперь b будет равно " + b);
 
+    //значение типа boolean может управлять оператором if
+    if (b)
+      System.out.println("Этот код выполняется");
+    b = false;
 
-    }
+    if (b)
+      System.out.println("Этот код не выполняется");
+
+    //результат сравнения типа boolean
+    System.out.println("10 > 9 будет равно " + (10 > 9));
+  }
 }
