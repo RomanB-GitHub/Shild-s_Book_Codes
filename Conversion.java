401a4f6a066ad1c115a802f0f000da51a8c50eb8
Roman Bovbalan <insekticud@gmail.com>
2021-10-26 10:12:48 +0300

    Was showed conversion of different types!

--- Conversion.java
+++ Conversion.java
@@ -1,23 +1,26 @@
+// Продемонстрировать приведение типов
+
 public class Conversion {
-    public static void main(String[] args) {
-        byte b;
-        int i = 257;
-        double d = 323.142;
 
-        //здесь деление по модулю даст 1 в остатке (в 257 влазит 1раз
-        // byte(256=1byte)  и остаток оттакого деления будет 1 ).
-        System.out.println("\nПреобразование типа int --> byte");
-        b = (byte) i;
-        System.out.println("i and b " + i + " --> " + b);
+  public static void main(String[] args) {
+    byte b;
+    int i = 257;
+    double d = 323.142;
 
-        // здесь дробная часть пройдет усечение, останется только целая!
-        System.out.println("\nПреобразование типа double --> int");
-        i = (int) d;
-        System.out.println("d and i " + d + " --> " + i);
+    System.out.print("***");
+    System.out.print("\nПреобразование типа int в byte.");
+    b = (byte) i;
+    System.out.println("i and b " + i + " -> " + b + ("произошло деление по модулю 257-256"
+        + " где 256 это от -128...0..до..127"));
 
-        //здесь будет и усечение и деление по модулю(остаток 67 !!!)
-        System.out.println("\nПреобразование типа double --> byte");
-        b = (byte) d;
-        System.out.println("d and b " + d + " --> " + b);
-    }
+    System.out.println("***");
+    System.out.println("Преобразование типа double в int");
+    i = (int) d;
+    System.out.println("d and i " + d + " -> " + i);
+
+    System.out.println("***");
+    System.out.println("Преобразование типа double в byte");
+    b = (byte) d;
+    System.out.println("d and b " + d + " -> " + b);
+  }
 }
