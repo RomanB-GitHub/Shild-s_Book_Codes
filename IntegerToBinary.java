6eaed9b9c38191f8b15ab99736e6a5e315e8fc64
Roman Bovbalan <insekticud@gmail.com>
2021-11-24 09:17:03 +0200

    New class IntegerToBinary shows transformation of the digits in to the binary string type...

---
+++ IntegerToBinary.java
@@ -0,0 +1,24 @@
+public class IntegerToBinary {
+
+  public static void main(String args[]) {
+
+    int a = 25;
+    int b = -25;
+    int c = 13;
+    int d = -13;
+    int e = 14;
+    int f = -14;
+    int g = 15;
+    int h = -15;
+    System.out.println(Integer.toBinaryString(a));
+    System.out.println(Integer.toBinaryString(b));
+    System.out.println(Integer.toBinaryString(c));
+    System.out.println(Integer.toBinaryString(d));
+    System.out.println(Integer.toBinaryString(e));
+    System.out.println(Integer.toBinaryString(f));
+    System.out.println(Integer.toBinaryString(g));
+    System.out.println(Integer.toBinaryString(h));
+
+  }
+
+}
