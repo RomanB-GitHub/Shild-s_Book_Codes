acb5fffa54d76c43d9d719ce578d63be8449ece2
Roman Bovbalan <insekticud@gmail.com>
2022-01-26 09:39:27 +0200

    New class "OverloadA" show how overloading process going on, if method signature different - will be overloading , if method signature same - will be overriding!!!

---
+++ OverloadA.java
@@ -0,0 +1,39 @@
+// Методы которые отличаются сигнатурами считаются
+// перегружаемыми, а не переопределенными!!!
+public class OverloadA {
+
+  int i, j;
+
+  OverloadA(int a, int b) {
+    i = a;
+    j = b;
+  }
+
+  //вывести содержимое переменной
+  void show() {
+    System.out.println("i and j : " + i + " " + j);
+  }
+}
+
+class OverloadB extends OverloadA {
+
+  int k;
+
+  public OverloadB(int a, int b, int c) {
+    super(a, b);
+    k = c;
+  }
+
+  void show(String msg) {
+    System.out.println(msg + k);
+  }
+}
+
+class OverloadAB {
+
+  public static void main(String[] args) {
+    OverloadB loadB = new OverloadB(6, 7, 8);
+    loadB.show("This is k = ");//вызов метода show() из класса OverloadB
+    loadB.show();//вызов метода show() из класса OverloadA
+  }
+}
